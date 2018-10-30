87c553141ce9981e2fe65077ced415bcfc5859b9
joost aafjes <joostaafjes@gmail.com>
2018-10-30 21:10:41 +0100

    add lesson 20

---
+++ 20-sigma-point-prediction/cmake-build-debug/CMakeFiles/20_sigma_point_prediction.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/20_sigma_point_prediction.dir/main.cpp.o"
+  "CMakeFiles/20_sigma_point_prediction.dir/ukf.cpp.o"
+  "20_sigma_point_prediction.pdb"
+  "20_sigma_point_prediction"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/20_sigma_point_prediction.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
