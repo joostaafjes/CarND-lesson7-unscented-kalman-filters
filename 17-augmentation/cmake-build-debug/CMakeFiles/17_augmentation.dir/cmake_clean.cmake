bc17eea8c0720a48623ebf67256c092eda9db4c2
joost aafjes <joostaafjes@gmail.com>
2018-10-30 17:06:51 +0100

    add 17

---
+++ 17-augmentation/cmake-build-debug/CMakeFiles/17_augmentation.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/17_augmentation.dir/main.cpp.o"
+  "CMakeFiles/17_augmentation.dir/ukf.cpp.o"
+  "17_augmentation.pdb"
+  "17_augmentation"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/17_augmentation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
