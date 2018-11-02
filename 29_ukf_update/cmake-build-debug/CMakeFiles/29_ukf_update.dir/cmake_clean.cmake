da1e9a63ed6d8f3b0c6cbc785be468e1e302f9f0
joost aafjes <joostaafjes@gmail.com>
2018-11-02 12:48:16 +0100

    add solution lesson 29

---
+++ 29_ukf_update/cmake-build-debug/CMakeFiles/29_ukf_update.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/29_ukf_update.dir/main.cpp.o"
+  "CMakeFiles/29_ukf_update.dir/ukf.cpp.o"
+  "29_ukf_update.pdb"
+  "29_ukf_update"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/29_ukf_update.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
